ff00da2a75d49a56ca9093286e653173a00d6ccf
shivam20042003 <129447005+shivam20042003@users.noreply.github.com>
2025-01-28 13:31:01 +0530

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import Header from '../components/Header/Header';
 import HeroSection from '../components/HeroSection/HeroSection';
 import FeaturedServices from '../components/FS/FeaturedServices';
 import Testimonials from '../components/Testimonials/Testimonials';
-import Footer from '../components/Footer/Footer';
+import Footer from '../components/footer/Footer';
 
 
 const HomePage = () => {
